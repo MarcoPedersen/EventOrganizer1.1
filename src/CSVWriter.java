6761dd4a02d2496b1f16b113ab8e01224c4e2562
MarcoPedersen <44088285+MarcoPedersen@users.noreply.github.com>
2018-11-26 11:10:20 +0100

    1

--- src/CSVWriter.java
+++ src/CSVWriter.java
@@ -1,2 +1,35 @@
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class CSVWriter {
+    public static void fileWriter(){
+        String filename = "desktop:test.csv";
+        try {
+            FileWriter fw = new FileWriter(filename);
+            Class.forName("com.mysql.jdbc-Driver").getConstructor().newInstance();
+            Connection conn =
+
+                    DriverManager.getConnection("jdbc:mysql://localhost:3306/event?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false", "brugernavn", "kode");
+            String query = "select * from testtable";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                fw.append(rs.getString(1));
+                fw.append(',');
+                fw.append(rs.getString(2));
+                fw.append(',');
+                fw.append(rs.getString(3));
+                fw.append('\n');
+            }
+            fw.flush();
+            fw.close();
+            conn.close();
+            System.out.println("CSV fil er blevet dannet lokalt på din desktop.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
