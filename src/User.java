951ea55b03559bbd5a13a9e569d042718c0c5c37
Nick Hansen <nick4630@stud.kea.dk>
2018-11-26 11:05:16 +0100

    sgfgsfsf

---
+++ src/User.java
@@ -0,0 +1,9 @@
+public class User {
+    static int id;
+    static String aDuration;
+    static String name;
+    static String type;
+    static String description;
+    static String additionalInfo;
+
+}
