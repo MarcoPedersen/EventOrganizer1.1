d9a1c3041074b03ac78d0c35c7e8d6256dc0e237
ThomasVindelev <44167383+ThomasVindelev@users.noreply.github.com>
2018-12-03 10:50:50 +0100

    hello :)

--- src/Csv.java
+++ src/Csv.java
@@ -12,7 +12,7 @@ public class Csv {
 
         try {
             try {
-                String arrfile ="resources/ExportArrangement.csv";
+                String arrfile = "resources/ExportArrangement.csv";
 
                 Database.connectToDatabase();
                 FileWriter fw = new FileWriter(arrfile);
@@ -53,30 +53,30 @@ public class Csv {
         try {
             try {
 
-        String evefile ="resources/ExportEvent.csv";
-        FileWriter fw = new FileWriter(evefile);
-        String query2 = "select * from `event`";
-        Statement st = Database.getConnect().createStatement();
-        ResultSet rs = st.executeQuery(query2);
+                String evefile = "resources/ExportEvent.csv";
+                FileWriter fw = new FileWriter(evefile);
+                String query2 = "select * from `event`";
+                Statement st = Database.getConnect().createStatement();
+                ResultSet rs = st.executeQuery(query2);
 
-            while (rs.next()) {
-                fw.append(rs.getString(2));
-                fw.append(',');
-                fw.append(rs.getString(3));
-                fw.append(',');
-                fw.append(rs.getString(4));
-                fw.append(',');
-                fw.append(rs.getString(5));
-                fw.append(',');
-                fw.append(rs.getString(6));
-                fw.append(',');
-                fw.append(rs.getString(7));
-                fw.append('\n');
-            }
+                while (rs.next()) {
+                    fw.append(rs.getString(2));
+                    fw.append(',');
+                    fw.append(rs.getString(3));
+                    fw.append(',');
+                    fw.append(rs.getString(4));
+                    fw.append(',');
+                    fw.append(rs.getString(5));
+                    fw.append(',');
+                    fw.append(rs.getString(6));
+                    fw.append(',');
+                    fw.append(rs.getString(7));
+                    fw.append('\n');
+                }
 
-            fw.flush();
-            fw.close();
-            st.close();
+                fw.flush();
+                fw.close();
+                st.close();
 
             } catch (IOException i) {
                 i.printStackTrace();
@@ -92,35 +92,36 @@ public class Csv {
         try {
             try {
 
-            String userfile = "resources/ExportUsers.csv";
-            FileWriter fw = new FileWriter(userfile);
-            String query3 = "select * from `users`";
-            Statement st = Database.getConnect().createStatement();
-            ResultSet rs = st.executeQuery(query3);
+                String userfile = "resources/ExportUsers.csv";
+                FileWriter fw = new FileWriter(userfile);
+                String query3 = "select * from `users`";
+                Statement st = Database.getConnect().createStatement();
+                ResultSet rs = st.executeQuery(query3);
 
-            while (rs.next()) {
-                fw.append(rs.getString(2));
-                fw.append(',');
-                fw.append(rs.getString(3));
-                fw.append(',');
-                fw.append(rs.getString(4));
-                fw.append(',');
-                fw.append(rs.getString(5));
-                fw.append('\n');
-            }
+                while (rs.next()) {
+                    fw.append(rs.getString(2));
+                    fw.append(',');
+                    fw.append(rs.getString(3));
+                    fw.append(',');
+                    fw.append(rs.getString(4));
+                    fw.append(',');
+                    fw.append(rs.getString(5));
+                    fw.append('\n');
+                }
 
-            fw.flush();
-            fw.close();
-            st.close();
+                fw.flush();
+                fw.close();
+                st.close();
 
             } catch (IOException i) {
                 i.printStackTrace();
             }
 
-            } catch (SQLException e) {
-                e.printStackTrace();
-         }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
+
     public static void importArrangement() {
 
         try {
@@ -130,19 +131,15 @@ public class Csv {
                 String read;
                 try {
                     read = bReader.readLine();
-                    if (read != null)
-                    {
+                    if (read != null) {
                         String[] array = read.split(",+");
                         Arrangement arrangement = new Arrangement(array[0], array[1], array[2], array[3], array[4]);
-                        Database.insertArrangement(arrangement," Din CSV fil er nu importeret", false);
+                        Database.insertArrangement(arrangement, " Din CSV fil er nu importeret", false);
                     }
                 } catch (IOException ex) {
                     ex.printStackTrace();
-                }
-                finally
-                {
-                    if (bReader == null)
-                    {
+                } finally {
+                    if (bReader == null) {
                         bReader.close();
                     }
                 }
@@ -151,6 +148,7 @@ public class Csv {
             ex.printStackTrace();
         }
     }
+
     public static void importEvent() {
 
         try {
@@ -160,19 +158,15 @@ public class Csv {
                 String read;
                 try {
                     read = bReader.readLine();
-                    if (read != null)
-                    {
+                    if (read != null) {
                         String[] array = read.split(",+");
                         Event event = new Event(array[0], array[1], array[2], array[3], array[4], array[5]);
                         Database.eventToDatabase(event);
                     }
                 } catch (IOException ex) {
                     ex.printStackTrace();
-                }
-                finally
-                {
-                    if (bReader == null)
-                    {
+                } finally {
+                    if (bReader == null) {
                         bReader.close();
                     }
                 }
@@ -181,4 +175,22 @@ public class Csv {
             ex.printStackTrace();
         }
     }
+
+    public static void importUsers() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("resources/ExportUsers.csv"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] array = line.split(",");
+                Admin admin = new Admin(array[0], array[1], array[2], array[3]);
+                Database.userToDatabase(admin, false);
+            }
+            br.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
+
+
